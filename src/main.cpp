45d0f848dbdae3ca9008896fd1c0253cef921f86
regalmoix <60090868+regalmoix@users.noreply.github.com>
2020-08-22 20:06:22 +0000

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "./global.h"
+#include <curses.h>
+
+int score = 0;
+
+using namespace std;
+std::vector<std::pair<int, int>> emptypos;
+
+int main()
+{
+    int n = 4;
+
+    vector<vector<int>> board(n, vector<int> (n,0));
+
+    boardinit(n);
+    
+    input(board, n);
+
+    printboard(board, n); 
+
+    return endwin();
+
+}
